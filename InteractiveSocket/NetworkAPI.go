ae91e1e50f9397f35eb92b14293e0175315567ee
oh eun Kwon <dhdms6512@gmail.com>
2018-11-25 03:54:24 +0900

    update

--- InteractiveSocket/NetworkAPI.go
+++ InteractiveSocket/NetworkAPI.go
@@ -1,21 +1,91 @@
 package InteractiveSocket
 
-import "strconv"
+import (
+	"fmt"
+	"net"
 
-type NodeData struct {
-	AndroidIP string;
-	HostName string;
-	Temp int;
-	Humidity int;
-	Gas int;
-	Light int;
+)
+
+func afterConnected(Android net.Conn, Node *NodeData)  {
+	flag := -3
+	var AndroidData *[]byte
+
+	for true {
+		AndroidData = COMM_RECVMSG(Android,0)
+
+			switch string(*AndroidData) {
+			case "HELLO":
+				if Node == nil{
+					COMM_SENDMSG("Require HostName",Android)
+					AndroidData = COMM_RECVMSG(Android,0)
+					Node = new(NodeData)
+					Node.HostName = string(*AndroidData)
+				}
+
+			case "BYE":
+				fmt.Println("SocketSVR Connection Closed (Cause : Client Request)")
+				flag = 0
+				break
+			}
+		if flag == 0{
+			break
+		}
+	}
 }
 
-func NewNodeData(hostname string,ip string, temp int, humidity int, gas int, light int) *NodeData {
-	NewNode := NodeData{HostName:hostname,AndroidIP:ip,Temp:temp,Humidity:humidity,Gas:gas,Light:light};
-	return &NewNode;
+func COMM_SENDMSG(msg string, Android net.Conn) string {
+
+	_, err := Android.Write([]byte(msg))
+	if err != nil{
+		return "SocketSVR While Send MSG (MSG :"+msg+")"
+	}
+	
+	return "netOK"
 }
 
-func(n *NodeData) PrintStatus() string {
-	return n.AndroidIP+n.HostName+strconv.Itoa(n.Temp)+strconv.Itoa(n.Humidity)+strconv.Itoa(n.Gas)+strconv.Itoa(n.Light);
+func COMM_RECVMSG(Android net.Conn, Count int) (*[]byte) {
+	var msg []byte
+	count := Count
+
+	if count > 4{
+		fmt.Println("SocketSVR Retrying read MSG ABORTED (Cause : count out)")
+		return nil
+	}
+	_,err := Android.Read(msg)
+	if err != nil {
+		var recover *[]byte
+		fmt.Println("SocketSVR Msg receive FAIL");
+		fmt.Println("SocketSVR Retrying read MSG")
+		recover = COMM_RECVMSG(Android,count)
+		if recover != nil {
+			return recover
+		}
+	}
+	return &msg
+}
+
+
+
+func main() int {
+	var Node *NodeData
+
+	Android, err := net.Listen("tcp",":6866")
+	if err != nil {
+		fmt.Println("SocketSVR Open FAIL")
+		return 1
+	}
+	defer Android.Close()
+
+	for true {
+		connect, err := Android.Accept()
+		if err != nil{
+			fmt.Println("SocketSVR TCP CONN FAIL")
+		}
+		defer connect.Close()
+
+		go afterConnected(connect, Node)
+	}
+
+
+	return 0
 }
--- InteractiveSocket/Node.go
+++ InteractiveSocket/Node.go
@@ -1,1 +1,16 @@
 package InteractiveSocket
+
+import "strconv"
+
+type NodeData struct {
+	AndroidIP string
+	HostName string
+	Temp int
+	Humidity int
+	Gas int
+	Light int
+}
+
+func(n *NodeData) PrintStatus() string {
+	return n.AndroidIP+n.HostName+strconv.Itoa(n.Temp)+strconv.Itoa(n.Humidity)+strconv.Itoa(n.Gas)+strconv.Itoa(n.Light)
+}
