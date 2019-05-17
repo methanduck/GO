d47ba69fdae76009c6bbca99b674ed3c1bdd671c
methanduck <dhdms6512@gmail.com>
2019-05-17 21:58:09 +0900

    update

--- InteractiveSocket/Starter/main.go
+++ InteractiveSocket/Starter/main.go
@@ -1,11 +1,20 @@
 package main
 
-import "runtime"
+import (
+	"flag"
+	"os/exec"
+	"runtime"
+)
 import (
 	"github.com/methanduck/GO/InteractiveSocket"
 )
 
 func main() {
+	addr, _ := exec.Command("awk `END{print $1}` /etc/hosts").Output()
+	if string(addr) == "" {
+		addr = []byte("127.0.0.1")
+	}
+	Address := flag.String("addr", string(addr), "")
 	runtime.GOMAXPROCS(runtime.NumCPU())
 
 	localServer := InteractiveSocket.Window{}
