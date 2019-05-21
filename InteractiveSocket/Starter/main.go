a167c5e79d6bf86c5092667218fb5a9f645922d5
methanduck <2595zaq!@>
2019-05-21 22:52:17 +0900

    update

--- InteractiveSocket/Starter/main.go
+++ InteractiveSocket/Starter/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"github.com/fatih/color"
 	"log"
+	"os"
+	"os/exec"
 	"runtime"
 )
 import (
@@ -11,36 +13,25 @@ import (
 
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
+	address := os.Getenv("windowaddr")
+	port := os.Getenv("windowport")
 
-	/* change flag to os.Getenv
-	addr, _ := exec.Command("sh", "-c", "awk 'END{print $1}' /etc/hosts").Output()
-	if string(addr) == "" {
-		addr = []byte("127.0.0.1")
+	if address == "" {
+		addr, err := exec.Command("/bin/sh", "-c", "awk 'END{print $1}' /etc/hosts").Output()
+		if err != nil {
+			color.Set(color.FgRed)
+			defer color.Unset()
+			log.Println("ERROR!! Socket server failed to get address or port!!")
+			log.Panic("Aborting initialize" + err.Error())
+		}
+		addrModified := addr[:len(addr)-1]
+		if err := run(string(addrModified), port); err != nil {
+			red := color.New(color.FgRed).SprintFunc()
+			log.Panic(red("Stop running" + err.Error()))
+		}
 	}
-	address := flag.String("addr", string(addr), "Set listening address")
-	port := flag.String("port", InteractiveSocket.SVRLISTENINGPORT, "Set listening port")
-	flag.Parse()
-	*/
-	/*
-		address := os.Getenv("windowaddr")
-		port := os.Getenv("windowport")
 
-		if address == "" {
-			addr, err := exec.Command("/bin/sh", "-c", "awk 'END{print $1}' /etc/hosts").Output()
-			if err != nil {
-				color.Set(color.FgRed)
-				defer color.Unset()
-				log.Println("ERROR!! Socket server failed to get address or port!!")
-				log.Panic("Aborting initialize" + err.Error())
-			}
-			addrModified := addr[:len(addr)-1]*/
-	if err := run("210.125.31.153", "6866"); err != nil {
-		red := color.New(color.FgRed).SprintFunc()
-		log.Panic(red("Stop running" + err.Error()))
-	}
 }
-
-//}
 func run(address string, port string) error {
 	localServer := InteractiveSocket.Window{}
 	return localServer.Start(address, port)
